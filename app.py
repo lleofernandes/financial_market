48df48f33fbf1f38d0fd6c225890e247b9023e31
lleofernandes <lf.04@hotmail.com>
2024-11-18 01:54:18 -0300

    test(dev): adjust file custom_class to get symbol list

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import MetaTrader5 as mt5
 import json
 import pytz
+import os
 from datetime import datetime, timedelta
 from graphics import create_graphics
 from reports import create_reports
 from etl import proccess_ativos
+
+from custom_class import custom_trader
 
 
 #Configuração de timezone e datas
@@ -12,6 +15,7 @@ timezone = pytz.timezone('America/Sao_Paulo') #timezone local
 end_date = datetime.now(timezone)
 start_date = end_date - timedelta(days=7)
 
+timeframe = 'TIMEFRAME_M1'
 
 #Lista de ativos
 ativos = ["AIEC11", "BTLG11", "MXRF11", "XPML11", ]
@@ -19,15 +23,22 @@ ativos = ["AIEC11", "BTLG11", "MXRF11", "XPML11", ]
 
 def main():
     try:
-        with open('credentials.json') as f:
-            data = json.load(f)            
+        file_path = os.path.join(os.getcwd(), 'credentials.json')
+        trader = custom_trader(file_path=file_path)
+        
+        # trader.update_ohlc(symbol='PETR3', timeframe='TIMEFRAME_M1')
+        trader.update_ohlc(ativos, timeframe, start_date, end_date, timezone)
+        
 
-        #inicializa a aplicação e valida os dados de acesso
-        if not mt5.initialize(login=data['mt_login'], password=data['mt_pswrd'], server=data['mt_server'], timeout=10000): #timeout é em milissegundos
+        # with open('credentials.json') as f:
+        #     data = json.load(f)            
+
+        # #inicializa a aplicação e valida os dados de acesso
         # if not mt5.initialize(login=data['mt_login'], password=data['mt_pswrd'], server=data['mt_server'], timeout=10000): #timeout é em milissegundos
-            print(f'initialize() failed, error code: {mt5.last_error()}')
-            mt5.shutdown()
-            raise SystemExit("Erro ao inicializar o MetaTrader 5. Verifique as credenciasis e o servidor.")
+        # # if not mt5.initialize(login=data['mt_login'], password=data['mt_pswrd'], server=data['mt_server'], timeout=10000): #timeout é em milissegundos
+        #     print(f'initialize() failed, error code: {mt5.last_error()}')
+        #     mt5.shutdown()
+        #     raise SystemExit("Erro ao inicializar o MetaTrader 5. Verifique as credenciasis e o servidor.")
         
         #Chama a funcao de ETL para processar os dados
         all_data = proccess_ativos(ativos, start_date, end_date, timezone)
--- custom_class.py
+++ custom_class.py
@@ -4,15 +4,26 @@ from datetime import datetime, timedelta
 import pandas as pd
 import os
 
-class btoolsTrader():
-    def __init__(self, file_path=None, login=None, pswrd=None, server=None):
+from etl import proccess_ativos
+
+
+initial_date = datetime(2020, 1, 1) #data fixa iniciando em 2020-1-1
+final_date = datetime.now()
+
+
+def initialize_directories(base_path, subfolders):
+    for folder in subfolders:
+        os.makedirs(os.path.join(base_path, folder), exist_ok=True)
+
+class custom_trader():
+    def __init__(self, file_path=None, login=None, password=None, server=None):
         if file_path:
             try:
                 with open(file_path) as f:
                     credentials = json.load(f)
                 self.server = credentials['mt_server']
                 self.login = credentials['mt_login']
-                self.pswrd = credentials['mt_pswrd']
+                self.password = credentials['mt_pswrd']
                 
             except:
                 print('Erro ao ler as credenciais')
@@ -21,13 +32,13 @@ class btoolsTrader():
         else:
             self.server = server
             self.login = login
-            self.pswrd = pswrd
-            if (login and pswrd and server) == None: 
+            self.password = password
+            if (login and password and server) == None: 
                 print("Erro ao ler as credenciais")
                 quit()
             
         #caso o mt5 não inicialize, quit()
-        if not mt5.initialize(login=self.login, pswrd=self.pswrd, server=self.server):
+        if not mt5.initialize(login=self.login, password=self.password, server=self.server):
             print(f'initialize() failed, error code: {mt5.last_error()}')
             mt5.shutdown()
             quit()
@@ -53,32 +64,63 @@ class btoolsTrader():
             'TIMEFRAME_H12': [mt5.TIMEFRAME_H12, 43200],
             'TIMEFRAME_D1': [mt5.TIMEFRAME_D1, 86400],
             'TIMEFRAME_W1': [mt5.TIMEFRAME_W1, 604800],
-            'TIMEFRAME_WN1': [mt5.TIMEFRAME_WN1, 2592000],         
+            'TIMEFRAME_MN1': [mt5.TIMEFRAME_MN1, 2592000],         
         }
 
-        if not os.path.isdir('ohlc'):
-            os.mkdir('ohlc')
-            for timeframe in self.timeframe_dict.keys():
-                try:
-                    os.mkdir(f"ohlc\\{timeframe}")
-                except FileExistsError:
-                    pass
-        elif not os.path.isdir('ticks'):
-            os.mkdir('ticks')
+
+        initialize_directories('ohlc', self.timeframe_dict.keys())
+        initialize_directories('', ['ticks'])
+        
+        # for folder in ['ohlc', 'ticks']:
+        #     os.makedirs(folder, exist_ok=True)
+
+        # for timeframe in self.timeframe_dict.keys():
+        #     os.makedirs(f"ohlc\\{timeframe}", exist_ok=True)       
+        
+        # if not os.path.isdir('ohlc'):
+        #     os.mkdir('ohlc')
+        #     for timeframe in self.timeframe_dict.keys():
+        #         try:
+        #             os.mkdir(f"ohlc\\{timeframe}")
+        #         except FileExistsError:
+        #             pass
+        # elif not os.path.isdir('ticks'):
+        #     os.mkdir('ticks')
 
     # funcoes ================
-    def update_ohlc(self, symbol, timeframe):
-        initial_date = datetime(2020, 1, 1) #data fixa iniciando por 2020
-        final_date = datetime.now()
+    def update_ohlc(self, ativos, timeframe, start_date, end_date, timezone=None):
+        """
+        Atualiza os dados OHLC para uma lista de ativos em um timeframe específico.
+        """
+
+        # Processa os ativos utilizando a função do ETL
+        all_data = proccess_ativos(ativos, start_date, end_date, timezone)
+
+        for df in all_data:
+            symbol = df['symbol'].iloc[0]
+            timeframe_path = f"ohlc\\{timeframe}\\{symbol}_{timeframe}.csv"
+
+            # Verifica se o arquivo já existe
+            if os.path.exists(timeframe_path):
+                df_old = pd.read_csv(timeframe_path, parse_dates=['date_time'])
+                # df_old['date_time'] = pd.to_datetime(df_old['date_time'])
+                df = pd.concat([df, df_old], ignore_index=True)
+                df.drop_duplicates(subset=['date_time', 'symbol'], inplace=True)
+
+             # Ordena e salva o resultado
+            df.sort_values(by='date_time', ascending=False, inplace=True)
+            df.to_csv(timeframe_path, index=False)
+            print(f"Dados atualizados para {symbol} no timeframe {timeframe}.")
+
 
         if not os.path.exists(f"ohlc\\{timeframe}\\{symbol}_{timeframe}.csv"):
-            df = pd.DateFrame(columns=['time', 'open', 'high', 'low', 'close', 'tick_volume', 'spread', 'real_volume'])
+            df = pd.DataFrame(columns=['date_time', 'open', 'high', 'low', 'close', 'tick_volume', 'spread', 'real_volume'])
         
         else:
             df = pd.read_csv(f"ohlc\\{timeframe}\\{symbol}_{timeframe}.csv")
-            df['time'] = pd.to_datetime(df['time'], unit='s')
-            if df['time'].max() < datetime.now() - timedelta(days=7):
-                initial_date = df['time'].max()
+            df['date_time'] = pd.to_datetime(df['date_time'])
+            if df['date_time'].max() < datetime.now() - timedelta(days=7):
+                initial_date = df['date_time'].max()
             else: 
                 return
             
@@ -87,51 +129,58 @@ class btoolsTrader():
         timeframe_name = timeframe
         timeframe = self.timeframe_dict[timeframe][0]
 
+        dfs = []
         while True:
-            data_aux = mt5.copy_rates_ranges(symbol, timeframe, initial_date, min(final_date_aux, final_date))
-            df_aux = pd.DataFrame(data_aux)
-            df_aux['time'] = pd.to_datetime(df_aux['time'], unit='s')
-            df = pd.concat([df_aux, df], ignore_index=True)
+            data_aux = mt5.copy_rates_range(symbol, timeframe, initial_date, min(final_date_aux, final_date))
+            if data_aux is not None:
+                df_aux = pd.DataFrame(data_aux)
+                df_aux['date_time'] = pd.to_datetime(df_aux['date_time'], unit='s')
+                dfs.append(df_aux)
+
 
             if final_date_aux > final_date:
                 break
-
-            initial_date = data_aux['time'].max()
+            
+            # if data_aux is not None and len(data_aux) > 0:
+            initial_date = pd.to_datetime(data_aux['date_time'].max(), unit='s')
             final_date_aux = initial_date + timedelta_default
 
-        df.sort_values(by='time', ascending=False, inplace=True)
+
+        if not df_aux.empty:
+            df = pd.concat(dfs, ignore_index=True)
+
+        df.sort_values(by='date_time', ascending=False, inplace=True)
         df.to_csv(f"ohlc\\{timeframe_name}\\{symbol}_{timeframe_name}.csv", index=False)
 
     def update_ticks(self, symbol):
-        initial_date = datetime(2020, 1, 1) #data fixa iniciando por 2020
-        final_date = datetime.now()
-
         if not os.path.exists(f"ticks\\{symbol}_ticksrange.csv"):
-            df = pd.DateFrame(columns=['time', 'bid', 'ask', 'last', 'volume', 'time_msc', 'flags', 'volume_real'])
+            df = pd.DataFrame(columns=['date_time', 'bid', 'ask', 'last', 'volume', 'time_msc', 'flags', 'volume_real'])
         
         else:
             df = pd.read_csv(f"ticks\\{symbol}_ticksrange.csv")
-            df['time'] = pd.to_datetime(df['time'], unit='s')
-            if df['time'].max() < datetime.now() - timedelta(days=7):
-                initial_date = df['time'].max()
+            df['date_time'] = pd.to_datetime(df['date_time'])
+            if df['date_time'].max() < datetime.now() - timedelta(days=7):
+                initial_date = df['date_time'].max()
         
         ticks_data = mt5.copy_ticks_range(symbol, initial_date, final_date, mt5.COPY_TICKS_TRADE)
         df_aux = pd.DataFrame(ticks_data)
-        df_aux['time'] = pd.to_datetime(df_aux['time'])
-        df = pd.concat([df_aux, df], ignore_index=True)
-        df['time'] = pd.to_datetime(df['time'])
+        df_aux['date_time'] = pd.to_datetime(df_aux['date_time'])
+        if not df_aux.empty:
+            df = pd.concat([df_aux, df], ignore_index=True)
+        df['date_time'] = pd.to_datetime(df['date_time'])
 
-        df.sort_values(by='time', ascending=False, inplace=True)
+        df.sort_values(by='date_time', ascending=False, inplace=True)
         df.to_csv(f"ticks\\{symbol}_ticksrange.csv", index=False)
 
     def slice(self, type, symbol, initial_date, final_date, timeframe=None):
         path = f'ohlc\\{timeframe}\\{symbol}_{timeframe}.csv' if type=='ohlc' else f'ticks\\{symbol}_ticksrage.csv'
+        
         if not os.path.exists(path):
             print(f'O ativo {symbol} não está registrado, crie utilizando a função o .update_{type}')
         else:
             df = pd.read_csv(path)
-            df['time'] = pd.to_datetime(df['time'])
-            return df.loc[(df['time'] >= initial_date) & (df['time'] < final_date)]
+            df['date_time'] = pd.to_datetime(df['date_time'])
+            return df.loc[(df['date_time'] >= initial_date) & (df['date_time'] < final_date)]
         pass
         
 
